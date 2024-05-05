d7f20da1097e5642f08188e75f27d79bcd5fb6ca
Thomas Barber <thomas.barber@sap.com>
2024-05-05 13:22:00 +0000

    Adding local build of PDF

---
+++ makepdf.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker run \
+       -e INPUT_INPUT_PATH="./" \
+       -e INPUT_OUTPUT_DIR="./" \
+       -e INPUT_IMAGES_DIR="./assets/img" \
+       -e INPUT_TEMPLATE="template.html" \
+       -e INPUT_IMAGE_IMPORT="./assets/img" \
+       -e INPUT_THEME="pdf.css" \
+       -v ${PWD}:"/github/workspace" \
+       ghcr.io/baileyjm02/markdown-to-pdf/markdown-to-pdf:latest
+
